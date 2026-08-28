a14d15a185fe47ed15aa456f3f4a553b29beb83c
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2026-08-28 21:06:36 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default async (req, res) => {
-  const { reqHandler } = await import('../dist/my-app/server/server.mjs');
-  return reqHandler(req, res);
+  const { app } = await import('../dist/goal-church-app/server/main.js');
+  return app(req, res);
 };
