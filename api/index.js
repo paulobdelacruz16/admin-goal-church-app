617e3eb09a1ca2c543f2b4357a50029b7f24be05
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2026-08-28 21:58:16 +0800

    update

--- api/index.js
+++ api/index.js
@@ -1,24 +1,20 @@
-export default async function handler(req, res) {
-  const { AppServerModule } = await import('../dist/goal-church-app/server/main.js');
-  const { ngExpressEngine } = await import('@nguniversal/express-engine');
-  const express = (await import('express')).default;
+import { join } from 'path';
+import express from 'express';
+import { ngExpressEngine } from '@nguniversal/express-engine';
+import { AppServerModule } from '../dist/goal-church-app/server/main.js';
 
-  const app = express();
+const app = express();
 
-  // Set up Angular Universal engine
-  app.engine('html', ngExpressEngine({ bootstrap: AppServerModule }));
-  app.set('view engine', 'html');
-  app.set('views', 'dist/goal-church-app/browser');
+app.engine('html', ngExpressEngine({ bootstrap: AppServerModule }));
+app.set('view engine', 'html');
+app.set('views', join(process.cwd(), 'dist/goal-church-app/browser'));
 
-  // Serve static files
-  app.get('*.*', express.static('dist/goal-church-app/browser', {
-    maxAge: '1y'
-  }));
+app.get('*.*', express.static(join(process.cwd(), 'dist/goal-church-app/browser'), {
+  maxAge: '1y'
+}));
 
-  // SSR routes
-  app.get('*', (req, res) => {
-    res.render('index', { req });
-  });
+app.get('*', (req, res) => {
+  res.render('index', { req });
+});
 
-  return app(req, res);
-}
+export default app;
