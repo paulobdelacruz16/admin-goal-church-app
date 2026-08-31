c7f49112c2810c600a6f8be06a45196012307a9c
paulobdelacruz16 <paulobdelacruz16@gmail.com>
2026-08-31 12:24:37 +0800

    update

--- routes.ts
+++ routes.ts
@@ -1,13 +1,9 @@
 import {ImageController} from './src/api/ImageController'
 import { Section1Controller } from './src/api/section1Controller'
-import { UploadImageController } from './src/api/uploadImageController'
-import { LoginCredentialController } from './src/api/loginCredentialController'
 import { DynmamicApiController } from './src/api/dynamicApiController'
 
 const imageController = new ImageController();
 const section1Controller = new Section1Controller();
-const uploadImageController = new UploadImageController();
-const loginCredentialController = new LoginCredentialController();
 const dynmamicApiController = new DynmamicApiController();
 
 export class Routes{
@@ -17,10 +13,7 @@ export class Routes{
         server.post('/api/section1', section1Controller.postData);
         server.put('/api/section1', section1Controller.updateData);
         server.delete('/api/section1/:id', section1Controller.deleteData);
-        server.post('/api/uploadImage',  uploadImageController.uploadImages);
         server.delete('/api/deleteImage/:name',  imageController.deleteImage);
-        server.post('/ng/api/findByloginCredential',  loginCredentialController.findByloginCredential);
-        server.get('/ng/api/getAlloginCredential',  loginCredentialController.findAllLoginCredential);
         server.get('/ng/api/dynamicApi/get/:url',  dynmamicApiController.getAllData);
         server.post('/ng/api/dynamicApi/update',  dynmamicApiController.updateData);
         server.post('/ng/api/dynamicApi/delete',  dynmamicApiController.deleteData);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { AdminComponent } from './components/admin/admin.component';
 
 const routes: Routes = [
   {
@@ -9,17 +8,6 @@ const routes: Routes = [
     component: MainComponent,
     loadChildren: () => import('./components/main/main.module')
       .then(mod => mod.MainModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./components/login/login.module')
-      .then(mod => mod.LoginModule)
-  },
-  {
-    path: 'admin',
-    component: AdminComponent,
-    loadChildren: () => import('./components/admin/admin.module')
-      .then(mod => mod.AdminModule)
   }
 ];
 
